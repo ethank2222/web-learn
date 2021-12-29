e518327736b3df16dcc9dcb44fad8b832eac287d
Ethan <59261562+ethank2222@users.noreply.github.com>
2021-12-29 10:33:04 -0800

    update1

---
+++ home.js
@@ -0,0 +1,5 @@
+window.onload = setTimeout(() => {window.scrollTo({
+    top: window.innerHeight-80,
+    left: 0,
+    behavior: 'smooth'
+})}, 2000);
--- main.js
+++ main.js
@@ -353,3 +353,19 @@ function resetAll(){
 	}
 }
 
+document.onload = function(){
+    var path = document.querySelector('.squiggle-container.path');
+    path.getTotalLength(); // 988.0062255859375
+    path.style.transition = path.style.WebkitTransition = 'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+    'stroke-dashoffset 2s ease-in-out';
+    // Go!
+    path.style.strokeDashoffset = '0';
+}
