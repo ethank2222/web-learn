cf8ecadae0ed618acb597a10b3d70c2177ce8b06
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2022-01-07 15:28:17 -0800

    Update 1/7

--- learn.js
+++ learn.js
@@ -1,3 +1,13 @@
+const info = {
+    'paragraph':{
+        'Name': 'Paragraph',
+        'Tag': '<p></p>',
+        'Description': 'This element allows you to add text to your webpage. Large chunks of information usually go in a paragraph.',
+        'More': 'https://www.w3schools.com/html/html_paragraphs.asp',
+        'Video': '<iframe width="560" height="315" src="https://www.youtube.com/embed/smaZvGlhJlQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    }
+}
+
 setInterval(function(){
     var current = document.getElementById("learn-search").placeholder;
     if (current == "Search."){
@@ -22,7 +32,28 @@ document.getElementById("search-button").addEventListener("click", event => {
 });
 
 function search() {
-    keyword = document.getElementById('learn-search').value;
-    const info = JSON.parse("info.JSON")
-    alert(info.name)
+    var keyword = document.getElementById('learn-search').value.toLowerCase();
+    var all = ['paragraph'];
+    if (all.includes(keyword) == false){
+        document.getElementById("result-header").innerText = "No results for '" + keyword + "'"
+        document.getElementById("result-description").innerText = "Common searches include 'paragraph' and 'header'"
+        document.getElementById('result-tag').innerText = "";
+        document.getElementById('result-more').innerText = "";
+        document.getElementById('result').lastChild.innerHTML = "";
+    }
+    else {
+        switch (keyword){
+            case ('paragraph'): {
+                obj = info.paragraph;
+            }
+            case ('header'): {
+                obj = info.paragraph;
+            }
+        }
+        document.getElementById('result-header').innerText = obj.Name;
+        document.getElementById('result-tag').innerText = obj.Tag;
+        document.getElementById('result-description').innerText = obj.Description;
+        document.getElementById('result-more').innerHTML = 'To learn more, see the W3Schools documentation <a href = ' + obj.More + ' class="link">here</a>.'
+        document.getElementById('result').lastChild.innerHTML = obj.Video;
+    }
 }
