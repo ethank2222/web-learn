ab81d86c8031c250361aa39f2d88cb8363c92339
Ethan <59261562+ethank2222@users.noreply.github.com>
2022-01-08 11:39:34 -0800

    Update learn.js

--- learn.js
+++ learn.js
@@ -5,6 +5,13 @@ const info = {
         'Description': 'This element allows you to add text to your webpage. Large chunks of information usually go in a paragraph.',
         'More': 'https://www.w3schools.com/html/html_paragraphs.asp',
         'Video': '<iframe width="560" height="315" src="https://www.youtube.com/embed/smaZvGlhJlQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    },
+    'header':{
+        'Name': 'Header',
+        'Tag': '<h1>Your paragraph text here.</h1>',
+        'Description': 'This element adds a header to your website. If you have multiple sections, use this element to separate each section with a title.',
+        'More': 'https://www.w3schools.com/tags/tag_header.asp',
+        'Video': '<iframe width="560" height="315" src="https://www.youtube.com/embed/s1jmDbZyEms" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     }
 }
 let all = ['paragraph', 'header'];
@@ -77,21 +84,32 @@ document.getElementById("search-button").addEventListener("click", event => {
 });
 
 function search() {
-    document.getElementById("list-options").remove();
     var keyword = document.getElementById('learn-search').value.toLowerCase();
     if (all.includes(keyword) == false){
-        document.getElementById("result-header").innerText = "No results for '" + keyword + "'"
-        document.getElementById("result-description").innerText = "Common searches include 'paragraph' and 'header'"
-        document.getElementById('result-tag').innerText = "";
-        document.getElementById('result-more').innerText = "";
-        document.getElementById('result-video').innerHTML = "";
+        if (document.getElementById("list-options") != null){
+            var obj = info[document.getElementById("list-options").firstChild.innerText];
+            document.getElementById('learn-search').value = document.getElementById("list-options").firstChild.innerText ;
+            document.getElementById('result-header').innerText = obj.Name;
+            document.getElementById('result-tag').innerText = obj.Tag;
+            document.getElementById('result-description').innerText = obj.Description;
+            document.getElementById('result-more').innerHTML = 'To learn more, see the W3Schools documentation <a href = ' + obj.More + ' class="link">here</a>.'
+            document.getElementById('result-video').innerHTML = obj.Video;
+        }
+        else{
+            document.getElementById("result-header").innerText = "No results for '" + keyword + "'"
+            document.getElementById("result-description").innerText = "Common searches include 'paragraph' and 'header'"
+            document.getElementById('result-tag').innerText = "";
+            document.getElementById('result-more').innerText = "";
+            document.getElementById('result-video').innerHTML = "";
+        }
     }
     else {
-        obj = info[keyword];
+        var obj = info[keyword];
         document.getElementById('result-header').innerText = obj.Name;
         document.getElementById('result-tag').innerText = obj.Tag;
         document.getElementById('result-description').innerText = obj.Description;
         document.getElementById('result-more').innerHTML = 'To learn more, see the W3Schools documentation <a href = ' + obj.More + ' class="link">here</a>.'
         document.getElementById('result-video').innerHTML = obj.Video;
     }
+    document.getElementById("list-options").remove();
 }
